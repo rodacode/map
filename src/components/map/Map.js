9cbf260336630cbbc7ea56f50002c9b66da1e1ff
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-12-14 21:12:44 +0100

    added marker and usePosition hook

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { usePosition } from '../../hooks/custom/usePosition';
 
 const Map = () => {
+    const { latitude, longitude } = usePosition();
+
 
     const [viewport, setViewport] = useState({
-        width: "800px",
-        height: "600px",
+        width: "100vw",
+        height: "100vh",
         latitude: 41.408697,
         longitude: 2.185137,
         zoom: 16
     });
-    
 
     return (
         <div>
+            <h1>COORDS: {latitude}</h1>
             <ReactMapGL
                 {...viewport}
                 mapStyle="mapbox://styles/invisibleobjects/ck45qnz0c1kbc1co205qlt6s2"
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                 onViewportChange={(viewport) => setViewport(viewport)}
             >
-                <Marker
-                    latitude={41.408697}
-                    longitude={2.185137}>
-                    <div><h4>HOLA SAMIGOZ</h4></div>
-                </Marker>
+                {latitude &&
+                    <Marker
+                        latitude={latitude}
+                        longitude={longitude}>
+                        HOLA
+            </Marker>}
             </ReactMapGL>
         </div>
     )
