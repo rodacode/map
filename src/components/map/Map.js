d9d17293dc30c97ce3f6b2f9e6d860fdc3638b1c
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-12-15 01:13:13 +0100

    play with map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import React, { useState, useEffect , useRef} from 'react';
+import ReactMapGL, { Marker, GeolocateControl } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { usePosition } from '../../hooks/custom/usePosition';
 import './map.scss';
@@ -14,8 +14,16 @@ const Map = () => {
         height: "100vh",
         latitude: 41.408697,
         longitude: 2.185137,
-        zoom: 14
+        zoom: 13
     });
+    useEffect(()=>{
+       //const map = Map.getMap();
+       // map.on('load', () => {
+         //add the GeoJSON layer here
+        // map.addLayer({...})
+       // console.log('piapaa')
+       // })
+    })
 
     return (
         <div>
@@ -27,11 +35,17 @@ const Map = () => {
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                 onViewportChange={(viewport) => setViewport(viewport)}
             >
+                        <GeolocateControl 
+          positionOptions={{enableHighAccuracy: true}}
+          trackUserLocation={true}
+        />
                 {data.markers.map(marker => (
                         <Marker
                         key={marker.id}
                         latitude={marker.latitude}
-                        longitude={marker.longitude}>HEY
+                        longitude={marker.longitude}>
+                            <img src='https://i.imgur.com/MK4NUzI.png' alt="marker icon"/>
+                            <span className="marker__name">{marker.name}</span>
                         </Marker>
                 ))
                 }
